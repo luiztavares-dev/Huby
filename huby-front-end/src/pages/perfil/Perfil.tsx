8165ed3b0189fc0672e497fed7516ab213411e9e
João Marcelo Alves <joao333marcelo@gmail.com>
2026-09-21 19:10:23 -0300

    starting perfil

--- huby-front-end/src/pages/perfil/Perfil.tsx
+++ huby-front-end/src/pages/perfil/Perfil.tsx
@@ -1,9 +1,22 @@
 import './Perfil.css'
 
 export function Perfil () {
+
+    const user = {
+        name: 'Lunah.xsz',
+        nameId: '#lunah.xsz',
+        bannerPerfil: 'banner-perfil.svg',
+        iconPerfil: 'icon-perfil.png'
+    }
+
     return (
         <>
-            <h1>Perfil</h1>
+            <img src={user.bannerPerfil} alt="banner do perfil" className="perfil-banner"/>
+            <div className="peril-ifcon">
+                <img src={user.iconPerfil} alt="ícone do perfil"/>
+                <h2>{user.name}</h2>
+                <p>{user.nameId}</p>
+            </div>
         </>
     )
 }
